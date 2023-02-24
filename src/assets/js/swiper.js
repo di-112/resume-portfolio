f0773ac6c65d968ba17648c126eb7395a20ced61
Dmitry Vezankin <di150499@gmail.com>
2023-02-24 18:09:09 +0300

    added adaptive

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -2,13 +2,21 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.brow
 
 new Swiper('.swiper', {
     spaceBetween: 24,
-    slidesPerView: 3,
+    slidesPerView: 1,
     autoplay: false,
+    breakpoints: {
+        960: {
+            slidesPerView: 2,
+        },
+        1520: {
+            slidesPerView: 3,
+        }
+    },
     navigation: {
         nextEl: '.works__button-next',
         prevEl: '.works__button-prev',
     },
     pagination: {
         el: '.swiper-pagination',
-    },
+    }
 });
